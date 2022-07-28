d513273adc0917c4d6bfa51cf42d8243f5164cca
admin <1547427276@qq.com>
2022-07-28 17:30:31 +0800

    sd

--- application/usercenter/controller/Index.php
+++ application/usercenter/controller/Index.php
@@ -8,6 +8,19 @@ class Index extends Base
 {
     public function Index()
     {
+        $where = [];
+
+        $username = $this->request->param('username');
+        $username && $where['username'] = ['like', '%'.$username.'%'];
+
+        //查询商户
+        $users = $this->modelUser->where(['status' => 1])->where($where)->paginate($this->request->param('limit', 10));
+
+        foreach ($users as $user){
+            $user->avatar = letter_avatar($user->username);
+        }
+
+        $this->assign('users', $users);
         $this->assign('user_info', $this->user);
         return $this->fetch('index');
     }
