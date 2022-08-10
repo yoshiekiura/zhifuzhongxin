353ae9b32977feb3142ed8a79bea77e3c274dadd
admin <1547427276@qq.com>
2022-08-10 18:50:04 +0800

    4

--- application/usercenter/controller/Index.php
+++ application/usercenter/controller/Index.php
@@ -4,23 +4,23 @@
 namespace app\usercenter\controller;
 
 
+use think\Request;
+
 class Index extends Base
 {
-    public function Index()
+    public function Index(Request $request)
     {
         $where = [];
-
-        $username = $this->request->param('username');
-        $username && $where['username'] = ['like', '%'.$username.'%'];
-
-        //查询商户
-        $users = $this->modelUser->where(['status' => 1])->where($where)->paginate($this->request->param('limit', 10));
-
-        foreach ($users as $user){
-            $user->avatar = letter_avatar($user->username);
+        if ($request->isAjax()){
+            $username = $this->request->param('username');
+            $username && $where['username'] = ['like', '%'.$username.'%'];
+            $users = $this->modelUser->where(['status' => 1])->where($where)->paginate($this->request->param('limit', 10));
+            foreach ($users as $user){
+                $user->avatar = letter_avatar($user->username);
+            }
+            return $this->success('success','', $users);
         }
 
-        $this->assign('users', $users);
         $this->assign('user_info', $this->user);
         return $this->fetch('index');
     }
