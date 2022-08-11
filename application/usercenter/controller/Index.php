db09d457c1254c165124bbec58806ff580e4245a
admin <1547427276@qq.com>
2022-08-11 19:07:03 +0800

    dfd

--- application/usercenter/controller/Index.php
+++ application/usercenter/controller/Index.php
@@ -10,39 +10,78 @@ class Index extends Base
 {
     public function Index(Request $request)
     {
-        $where = [];
-        if ($request->isAjax()){
-            $username = $this->request->param('username');
-            $createTime = $this->request->param('createTime');
-            $userType = $this->request->param('userType');
-            $username && $where['username'] = ['like', '%'.$username.'%'];
-            if ($createTime){
-                switch ($createTime){
-                    case 'w':
-                        $where['create_time'] = ['>', strtotime('-1 week')];
-                        break;
-                    case 'm':
-                        $where['create_time'] = ['>', strtotime('-1 month')];
-                        break;
-                    case '3m':
-                        $where['create_time'] = ['>', strtotime('-3 month')];
-                        break;
-                    case 'd':
-                        $where['create_time'] = ['>', strtotime('-1 year')];
-                        break;
-                }
-            }
-            $userType && $where['user_type'] = $userType;
-            $users = $this->modelPayCenterUser->where(['status' => 1])->where($where)->order('create_time desc')->paginate($this->request->param('limit', 10));;
-
-            foreach ($users as $user){
-                $user->avatar = letter_avatar($user->username);
-            }
-            return $this->success('success','', $users);
-        }
 
         $this->assign('user_info', $this->user);
         return $this->fetch('index');
+    }
+
+    /**
+     * 获取支付中心用户列表
+     * @throws \think\exception\DbException
+     */
+    public function getUsersLists()
+    {
+        $where = [];
+        $username = $this->request->param('username');
+        $createTime = $this->request->param('createTime');
+        $userType = $this->request->param('userType');
+        $username && $where['username'] = ['like', '%'.$username.'%'];
+        if ($createTime){
+            switch ($createTime){
+                case 'w':
+                    $where['create_time'] = ['>', strtotime('-1 week')];
+                    break;
+                case 'm':
+                    $where['create_time'] = ['>', strtotime('-1 month')];
+                    break;
+                case '3m':
+                    $where['create_time'] = ['>', strtotime('-3 month')];
+                    break;
+                case 'd':
+                    $where['create_time'] = ['>', strtotime('-1 year')];
+                    break;
+            }
+        }
+        $userType && $where['user_type'] = $userType;
+        $users = $this->modelPayCenterUser->where(['status' => 1])->where($where)->order('create_time desc')->paginate($this->request->param('limit', 10));;
+
+        foreach ($users as $user){
+            $user->avatar = letter_avatar($user->username);
+        }
+        return $this->success('success','', $users);
+    }
+
+    /**
+     * 获取渠道列表
+     * @throws \think\exception\DbException
+     */
+    public function getChannelLists()
+    {
+        $where = [];
+        $name = $this->request->param('name');
+        $createTime = $this->request->param('createTime');
+        $name && $where['name'] = ['like', '%'.$name.'%'];
+        if ($createTime){
+            switch ($createTime){
+                case 'w':
+                    $where['create_time'] = ['>', strtotime('-1 week')];
+                    break;
+                case 'm':
+                    $where['create_time'] = ['>', strtotime('-1 month')];
+                    break;
+                case '3m':
+                    $where['create_time'] = ['>', strtotime('-3 month')];
+                    break;
+                case 'd':
+                    $where['create_time'] = ['>', strtotime('-1 year')];
+                    break;
+            }
+        }
+        $channels =  $this->modelPayChannel->where(['status' => 1])->where($where)->order('create_time desc')->paginate($this->request->param('limit', 10));;
+        foreach ($channels as $channel){
+            $channel->avatar = letter_avatar($channel->name);
+        }
+        return $this->success('success','', $channels);
     }
 
     /**
