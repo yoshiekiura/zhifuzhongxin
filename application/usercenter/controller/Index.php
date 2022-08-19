19a0f3da28fae918976fbdcb48e29a5329c3e32a
admin <1547427276@qq.com>
2022-08-19 19:07:03 +0800

    487

--- application/usercenter/controller/Index.php
+++ application/usercenter/controller/Index.php
@@ -11,7 +11,40 @@ class Index extends Base
     public function Index(Request $request)
     {
 
+        $where = [];
+
+        $username = $request->param('username');
+        $createTime = $this->request->param('createTime', '');
+
+        $userType = $this->request->param('userType', '');
+        $username && $where['username'] = ['like', '%'.$username.'%'];
+        $userType && $where['user_type'] = $userType;
+        if ($createTime){
+            switch ($createTime){
+                case 'w':
+                    $where['create_time'] = ['>', strtotime('-1 week')];
+                    break;
+                case 'm':
+                    $where['create_time'] = ['>', strtotime('-1 month')];
+                    break;
+                case '3m':
+                    $where['create_time'] = ['>', strtotime('-3 month')];
+                    break;
+                case 'd':
+                    $where['create_time'] = ['>', strtotime('-1 year')];
+                    break;
+            }
+        }
+
+        $users = $this->modelPayCenterUser->where(['status' => 1])->where($where)->order('create_time desc')->paginate($this->request->param('limit', 12));;
+        foreach ($users as $user){
+            $user->avatar = letter_avatar($user->username);
+        }
         $this->assign('user_info', $this->user);
+        $this->assign('users', $users);
+        $this->assign('username', $username);
+        $this->assign('createTime', $createTime);
+        $this->assign('userType', $userType);
         return $this->fetch('index');
     }
 
